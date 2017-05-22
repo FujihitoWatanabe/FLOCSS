b2bb83fdc3b92cec1c592ef42883d2e5015e6cd3
DESKTOP-AIF8KO0\fuzih <fuzihito26@hotmailcom>
2017-05-23 00:28:21 +0900

    FLOCSS設計始め。

---
+++ public_html/gulpfile.js
@@ -0,0 +1,112 @@
+//gulpfile.js
+
+/*-------------------- インストールコマンド --------------------- */
+//グローバル
+  // npm i -g gulp
+//以下はプロジェクトフォルダで入力
+  // npm i -D gulp
+  // npm i -D gulp-autoprefixer
+  // npm i -D gulp-minify-css
+  // npm i -D gulp-uglify
+  // npm i -D gulp gulp-sass gulp-postcss postcss-cssnext
+  // npm i -D gulp-sass-glob
+  // npm i -D browser-sync
+  // npm i -D gulp-debug
+  // npm i -D gulp-plumber
+  // npm i -D gulp-notify
+  // npm i -D gulp-imagemin
+  // npm i -D gulp-ejs
+  // npm i -D gulp-minify-ejs
+  // npm i -D gulp-rename
+  // npm i -D gulp-pug
+  // npm i -D gulp-data
+/*-------------------- /インストールコマンド -------------------- */
+
+/*-------------------- plug-in -------------------------------- */
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var sass = require('gulp-sass');
+//var autoprefixer = require("gulp-autoprefixer");
+var postcss = require("gulp-postcss");
+var cssnext = require("postcss-cssnext");
+var sassGlob = require("gulp-sass-glob");
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+//var debug = require("gulp-debug");
+//var plumber = require("gulp-plumber");
+//var notify  = require("gulp-notify");
+var imagemin = require("gulp-imagemin");
+var pug = require("gulp-pug");
+var fs = require("fs");
+var data = require("gulp-data");
+/*-------------------- plug-in --------------------------------- */
+
+/*-------------------- タスク ---------------------------------- */
+//scssをコンパイル
+var paths = {
+  "scss": "src/scss/", //作業するscssのフォルダ
+  "css": "dest/css/"  //コンパイルして保存するcssのフォルダ
+}
+gulp.task('scss', function() {
+  var processors = [
+    cssnext({browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"]}) //ブラウザ/os バージョン
+  ];
+  return gulp.src(paths.scss + '**/*.scss')
+  .pipe(sassGlob({
+      ignorePaths: [
+        'foundation/_reset.scss'
+      ]
+  }))
+  .pipe(postcss(processors))
+  .pipe(sass({outputStyle: "expanded"}))
+  .pipe(gulp.dest(paths.css))
+});
+
+//画像圧縮
+gulp.task('imagemin', function(){
+  var srcGlob = 'src/images/**/*.+(jpg|jpeg|png|gif|svg)';
+  var dstGlob = 'src/images/';
+  var imageminOptions = {
+    optimizationLevel: 7
+  };
+  gulp.src(srcGlob)
+    .pipe(imagemin(imageminOptions))
+    .pipe(gulp.dest("dest/images/"));
+});
+
+//JS圧縮
+gulp.task("uglify", function() {
+  return gulp.src("src/js/*.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("dest/js/"))
+});
+
+//Pug(テンプレートエンジン)
+gulp.task("pug", function () {
+  //var json = JSON.parse(fs.readFileSync("./pages.json"));
+  gulp.src(
+    ["src/pug/**/*.pug",'!' + "src/pug/**/_*.pug"] //参照するディレクトリ、出力を除外するファイル
+  )
+  //.pipe(plumber())
+  .pipe(data( file => {
+    return JSON.parse(fs.readFileSync(`./pages.json`));
+  }))
+  .pipe(pug({
+    pretty:'    '
+  }))
+  .pipe(gulp.dest("dest/"))
+});
+/*-------------------- /タスク ------------------------------- */
+
+/*-------------------- リアルタイム監視------------------------ */
+gulp.task("watch", function() {
+  gulp.watch("src/scss/**/*.scss", ["scss"]);
+  gulp.watch("src/js/*.js", ["uglify"]);
+  gulp.watch("src/pug/**/*.pug", ["pug"]);
+  browserSync.init({
+    files: ["src/scss/**/*.scss","src/js/","src/pug/"],
+      proxy: "http://localhost/~flocss-dev/dest/",
+      //open: "external"
+  });
+});
+/*-------------------- /リアルタイム監視 ---------------------- */
---
+++ public_html/src/js/common.js
@@ -0,0 +1,2 @@
+console.log('しゅつりょく');
+console.log('しゅつりょく');
